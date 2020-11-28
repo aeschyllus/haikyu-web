f2e8b8dae210006b3f786c82decbcafc606feab8
aeschyllus <markanthony.lapada@tup.edu.ph>
2020-11-28 16:12:04 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        dimitri: ['Dimitri']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
